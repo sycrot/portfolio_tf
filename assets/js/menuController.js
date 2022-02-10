5a5cb81b23c418ee50c1b89fc3830c545c84468b
Thiago Farias <thiagofarias99@hotmail.com>
2022-02-10 20:15:16 -0300

    menuController functions  updated

--- assets/js/menuController.js
+++ assets/js/menuController.js
@@ -1,7 +1,12 @@
 const btnMenuBar = document.querySelector('.navbar--btnmenu i')
 const menuNavList = document.querySelector('.navlist')
 
-btnMenuBar.addEventListener('click', openMenu)
+let menuItems = document.querySelectorAll('.navlist--item-link[href^="#"]')
+let footerNavItems = document.querySelectorAll('.footer--ul-link[href^="#"]')
+
+let topWindow = 100
+
+renderEvents()
 
 function openMenu() {
     if(menuNavList.classList.contains('navlist-open')) {
@@ -9,5 +14,38 @@ function openMenu() {
     } else {
         menuNavList.classList.add('navlist-open')
     }
+}
+
+function renderEvents() {
+    btnMenuBar.addEventListener('click', openMenu)
+    menuItems.forEach(i => {
+        i.addEventListener('click', scrollToIdOnClick)
+    })
+    footerNavItems.forEach(i => {
+        i.addEventListener('click', scrollToIdOnClick)
+    })
+}
+
+function scrollToIdOnClick(event) {
+    event.preventDefault()
+    
+    const to = getScrollTopByHref(event.target)
+    scrollToPosition(to)
+    menuNavList.classList.remove('navlist-open')
+}
+
+function scrollToPosition(to) {
+    if (window.matchMedia('(min-width: 800px').matches) {
+        topWindow = 200
+    }
+    window.scroll({
+        top: to - topWindow,
+        behavior: 'smooth'
+    })
+}
+
+function getScrollTopByHref(el) {
+    const id = el.getAttribute('href')
+    return document.querySelector(id).offsetTop
 }
 
